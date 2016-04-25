d43f1fa2e70d574252a485b144fa058c474374f2
jd615645 <jd615645@gmail.com>
2016-04-25 23:48:33 +0800

    :cat2:hw3完成

--- DWP_hw3_4103056002/js/randrom.js
+++ DWP_hw3_4103056002/js/randrom.js
@@ -1,5 +1,6 @@
 var $mainBlock = document.querySelector('#main-block');
-var $rand_element = document.querySelectorAll('.rand');
+var $org_mainBlock = $mainBlock;
+var $main_element = document.querySelectorAll('.rand');
 var $btn_random = document.querySelector('#btn_random');
 var $btn_sort = document.querySelector('#btn_sort');
 var $btn_reset = document.querySelector('#btn_reset');
@@ -10,6 +11,8 @@ $btn_random.addEventListener('click', function(){
 });
 
 $btn_sort.addEventListener('click', function(){
+  clear_mainBlock();
+  sort_mainBlock();
 });
 
 $btn_reset.addEventListener('click', function(){
@@ -19,14 +22,25 @@ $btn_reset.addEventListener('click', function(){
 
 function rand_mainBlock() {
   var rand_num = [];
-  for (var i = 0; i < $rand_element.length; i++) 
+  for (var i = 0; i < $main_element.length; i++) 
     rand_num.push(i);
 
   //  random rand_num array
   rand_num.sort(function(){return Math.random()>0.5?-1:1;});
   
   for (var i = 0; i < rand_num.length; i++) {
-    $mainBlock.innerHTML = $mainBlock.innerHTML + $rand_element[rand_num[i]].outerHTML; 
+    $mainBlock.innerHTML = $mainBlock.innerHTML + $main_element[rand_num[i]].outerHTML; 
+  }
+}
+
+function sort_mainBlock() {
+  var sort_element = [];
+  for (var i = 0; i < $main_element.length; i++) {
+    sort_element.push(new wait_sort_element($main_element[i].innerText, i));
+  }
+  sort_element.sort(compare);
+  for (var i = 0; i < $main_element.length; i++) {
+    $mainBlock.innerHTML += $main_element[sort_element[i]['space']].outerHTML; 
   }
 }
 
@@ -35,7 +49,21 @@ function clear_mainBlock() {
 }
 
 function reset_mainBlock() {
-  for (var i = 0; i < $rand_element.length; i++) {
-    $mainBlock.innerHTML = $mainBlock.innerHTML + $rand_element[i].outerHTML; 
+  for (var i = 0; i < $main_element.length; i++) {
+    $mainBlock.innerHTML += $main_element[i].outerHTML; 
   }
+}
+
+function wait_sort_element(text, space) {
+  this.text = text;
+  this.space = space;
+}
+
+function compare(a,b) {
+  if (a.text < b.text)
+    return -1;
+  else if (a.text > b.text)
+    return 1;
+  else 
+    return 0;
 }
